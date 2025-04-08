dcdd231cebda9b974679e03edef8934fd6549a5c
Evelina <Evelinabum123@gmail.com>
2025-04-08 14:38:03 +0300

    up

---
+++ js/data/pizza.js
@@ -0,0 +1,34 @@
+export const foodList = [
+    {
+        photo : 'pizza.png',
+        name : 'Margarita',
+        price : 449,
+        availability : 4,
+    },
+    {
+        photo : 'pizza.png',
+        name : 'Havaju',
+        price : 899,
+        availability : 18,
+    }, {
+        photo : 'pizza.png',
+        name : '4 sezonu',
+        price : 1299,
+        availability : 1,
+    }, {
+        photo : 'pizza.png',
+        name : 'Ham & Mushrooms',
+        price : 2200,
+        availability : 5,
+    }, {
+        photo : 'pizza.png',
+        name : '4 suriu',
+        price : 1313,
+        availability : 13,
+    }, {
+        photo : 'pizza.png',
+        name : 'Mafija',
+        price : 1699,
+        availability : 0,
+    },
+];

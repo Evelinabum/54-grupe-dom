f7d08e02650c7ef384b04bdaf4837c61ef4d9525
Evelina <Evelinabum123@gmail.com>
2025-04-10 12:37:00 +0300

    up

--- js/components/header.js
+++ js/components/header.js
@@ -1,16 +1,16 @@
 export function header() {
     if (location.hostname !== 'localhost') {
-        const base = 'https://github.com/Evelinabum/54-grupe-dom/';
+        const base = 'https://github.com/Evelinabum.github.io/54-grupe-dom/';
         document.head.insertAdjacentHTML('afterbegin', `<base href="${base}">`);
     }
 
     const menu = [
-        { text: 'Home', href: '/' },
-        { text: 'Text', href: '/text' },
-        { text: 'Food', href: '/food' },
-        { text: 'Darzas', href: '/darzas' },
-        { text: 'Header', href: '/header' },
-        { text: 'Click', href: '/click' },
+        { text: 'Home', href: './' },
+        { text: 'Text', href: './text' },
+        { text: 'Food', href: './food' },
+        { text: 'Darzas', href: './darzas' },
+        { text: 'Header', href: './header' },
+        { text: 'Click', href: './click' },
     ];
 
     const lp = location.pathname;
